893ffca8e29344f87af2949914c846c15a0ec757
cizal <cizal@esignature.com.np>
2023-07-21 13:48:12 +0545

    made a separate Navbar component

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { AppBar, Button, IconButton, TextField, Toolbar, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import StoreIcon from '@mui/icons-material/Store';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function Navbar() {
+  return (
+    <AppBar position="relative">
+        <Toolbar sx={{ justifyContent: "space-between", bgcolor: "black" }}>
+          <Box sx={{ display: "flex" }}>
+            <StoreIcon sx={{ mr: 2, mt: 0.5 }} />
+            <Typography variant="h6" color="inherit" noWrap>
+              OnlineStore
+            </Typography>
+          </Box>
+          <Box>
+            <TextField
+              variant="outlined"
+              placeholder="Search"
+              size="small"
+              sx={{ bgcolor: "white", borderRadius: 2 }}
+            />
+            <IconButton type="submit">
+              <SearchIcon
+                sx={{
+                  color: "white",
+                }}
+              />
+            </IconButton>
+          </Box>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              bgcolor: "black",
+              "&:hover": {
+                backgroundColor: "#fff",
+                color: "black",
+              },
+            }}
+          >
+            <ShoppingCartIcon />
+            <p>10</p>
+          </Button>
+        </Toolbar>
+      </AppBar>
+  )
+}
+
+export default Navbar
